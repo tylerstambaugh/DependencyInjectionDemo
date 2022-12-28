b55bfb087b9d7182fde9fbab15e114b7a6631668
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2022-12-28 17:28:01 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using ConsoleUI;
+using DemoLibrary;
+
+var container = ContainerConfig.Configure();
+
+using (var scope = container.BeginLifetimeScope())
+{
+    var app = scope.Resolve<IApplication>();
+    app.Run();
+}
+
+Console.ReadLine();
